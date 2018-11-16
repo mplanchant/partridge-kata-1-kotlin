daa6a3902e578a8ccb0086555d0ce429682753de
Matthew Planchant <matt@logiccache.co.uk>
2018-11-16 10:08:31 +0000

    Adds gradle.properties.

--- src/main/kotlin/PartridgeMinesAPint.kt
+++ src/main/kotlin/PartridgeMinesAPint.kt
@@ -12,9 +12,8 @@ object PartridgeMinesAPint {
     )
 
     fun of(terms: List<String>): String {
-        val numberOfTerms = terms.count { term -> partridgeTerms.contains(term) }
-        return when {
-            (numberOfTerms < 1) -> "Lynn, I've pierced my foot on a spike!!"
+        return when (terms.count { term -> partridgeTerms.contains(term) }) {
+            (< 1) -> "Lynn, I've pierced my foot on a spike!!"
             else -> "Mine's a Pint${"!".repeat(numberOfTerms)}"
         }
     }
